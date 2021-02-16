8a56a0f13dd273d305e701be0bb0f6bccd3aeaa5
birzhan2 <79130940+birzhan2@users.noreply.github.com>
2021-02-16 13:22:14 +0600

    Add files via upload

---
+++ birzhan.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+if 1:
+    print("hello 1")
+
+
+# In[2]:
+
+
+a=3
+if a==3:
+    print("Здесь a=",a)
+
+
+# In[3]:
+
+
+if a>1:
+    print("hello 3")
+
+
+# In[4]:
+
+
+lst=[1,2,3]
+if lst:
+    print("hello 4")
+
+
+# In[5]:
+
+
+a=3
+if a>2:
+    print("H")
+else:
+    print("L")
+    
+
+
+# In[6]:
+
+
+a=17
+b=True if a>10 else False
+print(b)
+
+
+# In[ ]:
+
+
+a=int(input("9:"))
+if a<0:
+    print("Neg")
+elif a==0:
+        print("Zero")
+else: print("Pos") 
+        
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
